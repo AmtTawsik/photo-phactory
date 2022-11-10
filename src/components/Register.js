a1945349c42307d6a498cd8d8ad991bf257abed7
Abdullah Al Mubin <amttawsik99@gmail.com>
2022-11-10 16:47:51 +0600

    Finaly Done Maybe Last commit.

--- src/components/Register.js
+++ src/components/Register.js
@@ -108,6 +108,7 @@ const Register = () => {
                   type="text"
                   name="photo"
                   className="block w-full mt-1 p-2 border-gray-300 rounded-md shadow-lg text-black focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                  required
                 />
               </div>
             </div>
